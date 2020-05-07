103dbf1555438ad9591831aeebc0d7eefde14300
pashashmigol <pashashmigol@gmail.com>
2020-05-07 17:04:57 +0300

    remove hardcoded private key

--- radattribution-sdk-android/src/androidTest/java/com/rakuten/attribution/sdk/RAdAttributionTest.kt
+++ radattribution-sdk-android/src/androidTest/java/com/rakuten/attribution/sdk/RAdAttributionTest.kt
@@ -20,12 +20,18 @@ class RAdAttributionTest {
 
     @Before
     fun setUp() {
+        context = InstrumentationRegistry.getInstrumentation().context
+
+        val secretKey = context.assets
+            .open("private_key")
+            .bufferedReader()
+            .use { it.readText() }
+
         val configuration = Configuration(
             appId = "com.rakuten.advertising.RADAttribution-Example",
             privateKey = secretKey,
             isManualAppLaunch = false
         )
-        context = InstrumentationRegistry.getInstrumentation().context
         attribution = RAdAttribution(context, configuration)
     }
 
@@ -70,7 +76,7 @@ class RAdAttributionTest {
                     deviceId = "00000000-0000-0000-0000-000000000000",
                     osVersion = "10"//os version without "." causes an error for now
                 )
-        ){
+        ) {
             deferredResult.complete(it)
         }
         val result = deferredResult.await()
@@ -88,7 +94,7 @@ class RAdAttributionTest {
                 .copy(applicationId = "com.rakutenadvertising.RADAdvertiserDemo"),
             deviceData = DeviceData.create(context)
                 .copy(os = "iOS")
-        ){
+        ) {
             deferredResult.complete(it)
         }
 
@@ -109,7 +115,7 @@ class RAdAttributionTest {
                 .copy(os = "iOS"),
             customData = mapOf("key_1" to "value_1", "key_2" to "value_2", "key_3" to "value_3"),
             customItems = arrayOf("item_1", "item_2", "item_3", "item_4", "item_5")
-        ){
+        ) {
             deferredResult.complete(it)
         }
 
@@ -139,62 +145,11 @@ class RAdAttributionTest {
                 .copy(applicationId = "com.rakutenadvertising.RADAdvertiserDemo"),
             deviceData = DeviceData.create(context)
                 .copy(os = "iOS")
-        ){
+        ) {
             deferredResult.complete(it)
         }
 
         val result = deferredResult.await()
         assertTrue(result is Result.Success)
     }
-
-    private val secretKey =
-        "MIIJKAIBAAKCAgEAvYkQBxCX6fDYHIzHDmJWv7Ic0Ab9f62phB2CfvG5JIvTC3Ur" +
-                "Lxta7uzm2GhJhACu0QV6K+cTX5J6jTBrHTwlWr8Eqsen+evMET9TxdRUl5r1Wl90" +
-                "RjqVjXDKLGieziouxzg9O0akjwLMoKSsDNk/qGtcjnkUKnQlcox+b5ruFuZnnaxS" +
-                "qZHhzs5h0ejE9Eum1Eq/6uaz6xPCisf8Ax8NC4La7c3InQ0VWwh724adLI3zMvMG" +
-                "FJIKhHFidtxSep93g9qgwEc9iswdnywsWWRWqcQs62a/OBo/zevZT5g/524uKYgf" +
-                "hQbJLlAn83NXOrK3WFNcGXfGhzy1ispqDsyyQtrgjnvJUlbTNBtiBxTbsIxU6Cxv" +
-                "Unw5RDePUtd460L4MoVgJjrIJfNYa5g1YZIK8AyPiuEvW3JufFoHyrZ2basavGM1" +
-                "G7MTwLx+ZtD4jC65VEAo0kptQXWxruhvodlg8AMm4FOX6fumsD0ImP1c+goP6a4D" +
-                "TxeA8BFAqAIx1SDSdMMUUGTVmJvSR7p8bcjOtj5+El5wGH46PyvpyiP0vNB6FxYP" +
-                "jQr6V2o3OfsT4PbOQYOhHFDFcfKLcinqtHW3/hOUekom+VAKEXjM1px8/bJudc7H" +
-                "I9+a86B1TFW2mK/reiINT3ZrIbaHnqPGWmkknr2AGPwEpE/DkW8LvU891W0CAwEA" +
-                "AQKCAgBpdgR3CeKdhyeY6zQvasR+MasajWksTAMQwiLEY9fy3+J0c6OtuHjmjOb9" +
-                "7zlIu+CJ6ZRLLW54NVb/jLttLvRSBAuiwylSRMPtrOD+KOFQ4iY3PPnDwgFJDENS" +
-                "ZnxGlu4kZ8SaYPpboOEfWcFp/NAQ9HwxwmlYHfxgOpB3pStpjpaFA0eTltqgafHA" +
-                "DNbaX+XaJiWXnPrriLks4430ZqipiQwsWd6QlKEXYCcaxVJbYji6VsNBWumDPFvf" +
-                "a0Rxep3TvijIFFvICT9KPBgJPW2DVObxrOAlZWWvPNZUFZEpQwNolJeFO3thy7QP" +
-                "IFSfEqY1/Vw3x4+t3DffnDVbOsbMusEvBllXv3P+6y581qoeP7Cwe85/gIqCg9KV" +
-                "7URDUEeFOLIAX7zB2rwQVCc7sgbQcFw7wxQ6e66VnRWjFWU14RrQ+5Nd72v33Pp4" +
-                "gcWU0Aplz74mvw7ml2nMylPfCn9P8xkk6CYDPz8EVkpNofw2ypCztw1jlYxvwpQF" +
-                "OPrTmEMChZIu6VsxoxcgwjWvxNg8ZTCily3rqJdlr39J/2mFzgDMy2s7SYUER+QC" +
-                "vE1DCk3BBaCfxiI6SQu+QXqTLsLpJGzjVnovpVUgvm4SBrp52w9oAjr8DESRj9Zq" +
-                "8bjy2WmaPzZPh+ni0R8OfGpzZp4Xb3eZ0rAO90uMLu6f6dxzeQKCAQEA6qxpn0YV" +
-                "11m+kvkzdGuJXcZDxVBY8oqmh2q+IIDQl48L7kho0AbMQrpfR8Ychic4GoVPmRdb" +
-                "JIs02Xv5H5orAwsoqmW9W+Mm47pSdkGLddtcelltiD1+CyoYvuQlIARuxUCnLI1y" +
-                "FBwmTbmQQPVFVdiwndDARRc3I0oXMNWvhAWr3jBHdpkMSJP5EoZmv/pFWVkOxrxt" +
-                "cEU2xUUEgJ/0d3cMrid0TcB6IM8+fYPucvTRDuuG9Z0wPaaxwYCqAc7Njb4xjKNc" +
-                "1KpFyTQzy3NHgciZss72FetuLJweX389d+lFXhzz1KD2euWrOk7ppLE2d9k+gfFy" +
-                "+2h6hu+AMY4iAwKCAQEAzsKHrnipxPZqr3I4N/hhojrnUPVOts7fokNfmcN9jYbZ" +
-                "kAksJFzIAm6KrU0qnQlQz1da5DzqqyMcvLcBi1ha7iVhm9P6K5hH5l5qgUiOa9i3" +
-                "v0R824g4homikrtB4Lp16ILxaR96J/iXSsq+y/og6I/qJLsgCTZ6wE4YbYD0/ynM" +
-                "IiT3/Ktc1pnIMfKdpJhIa6GN+hDZVA3Iw2NgAhRmAvQcKmGOS3fMYCnrSRtqfnPG" +
-                "5LZxsB28xsfq1/lY6mXXcM3gJKimL/F+PeQHuEfy8kkH3KdqhKTvnBaoRHNVTiIb" +
-                "xIK5hOJs+jgL2Jq5um3x+FoHZ4OoylkvAsR61aDHzwKCAQAgpHLcsMcA+X7Eut0p" +
-                "aHvnC1kJ7S8yLY8UbwibRM+/BSrHrlLF/OwUrA/sz+XP00y+g6SayuDmqGZlihUR" +
-                "DETHW5oAeb5pNaOHMbees2dOsYCflCjkNol9zBE9HEb9uSAfV+rpC5O+sFuznAgw" +
-                "wO0wD8Ahc5QLCDunMPsg09hiKNfLRDPsj0ViIxMWPJO2SH20++pOQo+Gelov/nWt" +
-                "3pIGvAyLfPl0hz11qt4qX1ufqeYaiTBwobjAGpvHKrp7HeUBvl7uDRswia7DWfuK" +
-                "ZTKhHuIiOR+J1QGyOtUOu4g1UcFQYf6YqPsgBSpYJfnh6rSE3zcOpCM2TUYd1tqi" +
-                "Cf85AoIBAQCfKZztMDHwT4kc+h5Q58Gw8wsyhURM8b+x1492fMjf89jzSjxS2aGW" +
-                "TaYvdmHBdXRhyGtNm59CkssCcxabQC7veJNFM883VAi1TCVM1J/eYXxBnuVG0fxB" +
-                "hR5DOieiaaduj65rMDIHJxTAHIb32tsObArgr7Qfo3KnKvcfBNCUxIZCGpdUSE41" +
-                "XTiBSrUUCa5mPH1g7St/ywSrdIppz24gA+7SqTqy2cvYkyxuoM4//bw0QEYQPzQd" +
-                "CbS9AVPzTOamDbXoQnN8ILj/x9QxhiF9Zb7Jm48iAR362G48E1Styw/5HHDX3L3r" +
-                "eM2VtrYWz6AfgJ6GjxGWg0TvKnUskpJ9AoIBAEBbqXLqIKCiJ9TLDkijk/xw7Yhk" +
-                "A6nTiOwXCtA2b+ZNZolEypGM1EDBLXutPHUXETMAVziSq22kYAaRKFtOxf/ZKdiM" +
-                "eQ367p75KyHtLdR2+mbO206ddYq2hfnpc0ABdHy9yJQWmqSytpL7LB1lzOZB054c" +
-                "koJtXaRXMNe5qwmnMKerhHUWPlIPcDB1U+W9sOgy1aTiQBdIGTvSdvUYJyJ/JeL9" +
-                "FMkTsUmZXtpGPBeAKAZgNz3BnBJu8JU8ASA1eHDvhIPHg6PUkDHx5X7X/+xQGW1N" +
-                "m6n6UBfw9EqrngJNmdJuk1BqQgtNIUj9Ml2UFYfP8xON98PKtXx1zYLZPfo="
 }
