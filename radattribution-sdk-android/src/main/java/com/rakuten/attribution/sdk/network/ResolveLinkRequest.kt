6f9efa83767d3798c7f78e98cc4ce750bd2a7e0c
ts-pavlo.shmyhol <pashashmigol@gmail.com>
2020-09-30 15:03:09 +0300

    set of fixes and improvements

--- radattribution-sdk-android/src/main/java/com/rakuten/attribution/sdk/network/ResolveLinkRequest.kt
+++ radattribution-sdk-android/src/main/java/com/rakuten/attribution/sdk/network/ResolveLinkRequest.kt
@@ -7,7 +7,7 @@ import com.squareup.moshi.Json
 internal data class ResolveLinkRequest(
     @Json(name = "first_session") val firstSession: Boolean,
     @Json(name = "link_identifier") val linkIdentifier: String = "",
-    @Json(name = "android_app_link_ url") val appLinkUrl: String = "",
+    @Json(name = "android_app_link_url") val appLinkUrl: String = "",
     @Json(name = "user_data") val userData: UserData,
     @Json(name = "device_data") val deviceData: DeviceData
 )
